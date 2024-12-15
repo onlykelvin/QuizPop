b18d50ceb9e49376f3dd6088ee167a6ab55d20a5
onlykelvin <143399058+onlykelvin@users.noreply.github.com>
2024-12-15 03:02:39 +0100

    Update categoryUtils.ts

--- src/utils/categoryUtils.ts
+++ src/utils/categoryUtils.ts
@@ -1,5 +1,5 @@
 import { Category, Question } from '../types';
-import i18n from '../i18n';
+import i18n from './i18n';
 
 export async function loadAllCategories(): Promise<Category[]> {
   const categories = [
