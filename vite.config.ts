f7db05ef5d4313ed6b2b3118ac1a6e9a9fe8da8c
onlykelvin <143399058+onlykelvin@users.noreply.github.com>
2025-01-25 16:31:38 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // Ensure correct relative paths
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
